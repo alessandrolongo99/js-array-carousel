fbfe6cc340aa3c351c3656757a58a06eacdf438e
alessandrolongo99 <longoalessandro153@gmail.com>
2022-06-20 16:17:54 +0200

    problemi milestone 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-images = [
+const images = [
     'https://cdn.photographycourse.net/wp-content/uploads/2022/04/Portrait-vs-Landscape-Featured-Image-3.jpg',
     'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg',
     'https://cdn.photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg',
@@ -6,3 +6,12 @@ images = [
     'https://iso.500px.com/wp-content/uploads/2021/02/Torres-Del-Paine-Sunset-By-Paul-Reiffer-2-1500x1000.jpg',
     'https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg'
 ];
+
+const container = document.getElementById('ms_img_container');
+
+for (let i = 0; i < images.length; i++) {
+    const img = document.createElement('img');
+    img.className('d-none w-100');
+    img.src(images[i]);
+    container.appendChild(img);
+}
